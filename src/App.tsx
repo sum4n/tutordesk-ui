f106a6ba4e40a4bf38ae35b886ddc4206b133074
Suman Das <54246298+sum4n@users.noreply.github.com>
2026-09-06 22:42:03 +0530

    feat: remove boilerplate code and render sidebar and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import { Button } from "@/components/ui/button"
+import Sidebar from "@/components/Sidebar"
+import Dashboard from "@/components/Dashboard"
 
 export function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
+    <div className="flex p-2">
+      <div className="h-screen basis-1/4">
+        <Sidebar />
+      </div>
+      <div className="h-screen basis-3/4">
+        <Dashboard />
       </div>
     </div>
   )
