87d65215ea5c05fa87fb54ea61828dc6c05564e2
Suman Das <54246298+sum4n@users.noreply.github.com>
2026-09-06 22:41:38 +0530

    feat: create dashboard

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+function Dashboard() {
+  return (
+    <main>
+      <header className="border-b-2">
+        <h1>Dahboard Header</h1>
+      </header>
+      <section></section>
+    </main>
+  )
+}
+
+export default Dashboard
